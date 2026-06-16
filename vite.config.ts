1530ef5dad08579a3de263eb03d06d8f184cb7ac
ch1acosine <162133422+ch1acosine@users.noreply.github.com>
2026-06-16 09:24:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/New-Exam-Clock/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
